1a93115688e2b0cd2c7a3c22fd5aac75a557740c
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-08-06 16:54:52 +0100

    tidying some of the code

--- gender-refs.py
+++ gender-refs.py
@@ -23,49 +23,48 @@ def load_bibtex(filename):
         sys.exit('ERROR: Bibtex file appears to be incorrectly formatted.\n'
                  'Check for missing brackets around dates or mismatched brackets.')
 
-genders = []
 
 load_bibtex(sys.argv[1])
 headers = {'User-Agent': 'GenderCheck/0.1 (https://swdg.io; mailto:{})'.format(load_email())}
 
-#
-# d = gender.Detector(case_sensitive=False)
-#
-# print('\n\n\tFound {} references.'.format(len(refs.entries)))
-# print('\tReferences without a DOI cannot be processed.\n\n')
-#
-# for ref in refs.entries:
-#     try:
-#         doi = url.quote(ref['doi'])
-#         query = 'https://api.crossref.org/works/{}'.format(doi)
-#         r = requests.get(query, headers=headers)
-#         try:
-#             for author in r.json()['message']['author']:
-#                 name = author['given']
-#                 name = HumanName(name)
-#
-#                 # Check for people with a first initial rather than a first name
-#                 if name.is_an_initial(name.first):
-#                     # These people may use a middle name as their given name
-#                     if len(name.middle) > 1:
-#                         genders.append(d.get_gender(name.middle))
-#                 else:
-#                     # Seems like a first name we can process
-#                     genders.append(d.get_gender(name.first))
-#
-#         except:
-#             # No author record - possibly citing an organisation, or invalid DOI
-#             pass
-#
-#     except:
-#         # Call to Crossref has failed in a bad way
-#         pass
-#
-# counts = Counter(genders)
-# total_count = 0
-#
-# for c in counts.items():
-#     print(output_formatter(*c))
-#     total_count += c[1]
-#
-# print('Total references processed: {}\n\n'.format(total_count))
+d = gender.Detector(case_sensitive=False)
+genders = []
+
+print('\n\n\tFound {} references.'.format(len(refs.entries)))
+print('\tReferences without a DOI cannot be processed.\n\n')
+
+for ref in refs.entries:
+    try:
+        doi = url.quote(ref['doi'])
+        query = 'https://api.crossref.org/works/{}'.format(doi)
+        r = requests.get(query, headers=headers)
+        try:
+            for author in r.json()['message']['author']:
+                name = author['given']
+                name = HumanName(name)
+
+                # Check for people with a first initial rather than a first name
+                if name.is_an_initial(name.first):
+                    # These people may use a middle name as their given name
+                    if len(name.middle) > 1:
+                        genders.append(d.get_gender(name.middle))
+                else:
+                    # Seems like a first name we can process
+                    genders.append(d.get_gender(name.first))
+
+        except:
+            # No author record - possibly citing an organisation, or invalid DOI
+            pass
+
+    except:
+        # Call to Crossref has failed in a bad way
+        pass
+
+counts = Counter(genders)
+total_count = 0
+
+for c in counts.items():
+    print(output_formatter(*c))
+    total_count += c[1]
+
+print('Total references processed: {}\n\n'.format(total_count))
