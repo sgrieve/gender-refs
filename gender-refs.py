9c41ce82ea0078c49ffadfc2cbd6edb43d036e9f
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-08-06 16:53:38 +0100

    adding some error handling for the file reading.

--- gender-refs.py
+++ gender-refs.py
@@ -1,5 +1,6 @@
 import bibtexparser
 import requests
+import sys
 import urllib.parse as url
 import gender_guesser.detector as gender
 from collections import Counter
@@ -12,52 +13,59 @@ def load_email():
 def output_formatter(category, count):
     return '{}: {}'.format(category.capitalize().replace('_', ' '), count)
 
-d = gender.Detector(case_sensitive=False)
-
-with open('references.bib') as bibtex_file:
-    refs = bibtexparser.load(bibtex_file)
+def load_bibtex(filename):
+    try:
+        with open(filename) as bibtex_file:
+            return bibtexparser.load(bibtex_file)
+    except FileNotFoundError:
+        sys.exit('ERROR: Bibtex file, {}, not found.'.format(filename))
+    except KeyError:
+        sys.exit('ERROR: Bibtex file appears to be incorrectly formatted.\n'
+                 'Check for missing brackets around dates or mismatched brackets.')
 
 genders = []
 
-email = load_email()
+load_bibtex(sys.argv[1])
+headers = {'User-Agent': 'GenderCheck/0.1 (https://swdg.io; mailto:{})'.format(load_email())}
 
-headers = {'User-Agent': 'GenderCheck/0.1 (https://swdg.io; mailto:s@swdg.io)'}
-
-print('\n\n\tFound {} references.'.format(len(refs.entries)))
-print('\tReferences without a DOI cannot be processed.\n\n')
-
-for ref in refs.entries:
-    try:
-        doi = url.quote(ref['doi'])
-        query = 'https://api.crossref.org/works/{}'.format(doi)
-        r = requests.get(query, headers=headers)
-        try:
-            for author in r.json()['message']['author']:
-                name = author['given']
-                name = HumanName(name)
-
-                # Check for people with a first initial rather than a first name
-                if name.is_an_initial(name.first):
-                    # These people may use a middle name as their given name
-                    if len(name.middle) > 1:
-                        genders.append(d.get_gender(name.middle))
-                else:
-                    # Seems like a first name we can process
-                    genders.append(d.get_gender(name.first))
-
-        except:
-            # No author record - possibly citing an organisation, or invalid DOI
-            pass
-
-    except:
-        # Call to Crossref has failed in a bad way
-        pass
-
-counts = Counter(genders)
-total_count = 0
-
-for c in counts.items():
-    print(output_formatter(*c))
-    total_count += c[1]
-
-print('Total references processed: {}\n\n'.format(total_count))
+#
+# d = gender.Detector(case_sensitive=False)
+#
+# print('\n\n\tFound {} references.'.format(len(refs.entries)))
+# print('\tReferences without a DOI cannot be processed.\n\n')
+#
+# for ref in refs.entries:
+#     try:
+#         doi = url.quote(ref['doi'])
+#         query = 'https://api.crossref.org/works/{}'.format(doi)
+#         r = requests.get(query, headers=headers)
+#         try:
+#             for author in r.json()['message']['author']:
+#                 name = author['given']
+#                 name = HumanName(name)
+#
+#                 # Check for people with a first initial rather than a first name
+#                 if name.is_an_initial(name.first):
+#                     # These people may use a middle name as their given name
+#                     if len(name.middle) > 1:
+#                         genders.append(d.get_gender(name.middle))
+#                 else:
+#                     # Seems like a first name we can process
+#                     genders.append(d.get_gender(name.first))
+#
+#         except:
+#             # No author record - possibly citing an organisation, or invalid DOI
+#             pass
+#
+#     except:
+#         # Call to Crossref has failed in a bad way
+#         pass
+#
+# counts = Counter(genders)
+# total_count = 0
+#
+# for c in counts.items():
+#     print(output_formatter(*c))
+#     total_count += c[1]
+#
+# print('Total references processed: {}\n\n'.format(total_count))
