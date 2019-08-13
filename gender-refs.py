d020dc9d29b5284be0b33a8dbeecefcde0493831
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-08-13 11:39:11 +0100

    fixed async queries hitting the rate limit by batching the parallel requests into blocks of 25, could increase to 50 but no need to take the piss

--- gender-refs.py
+++ gender-refs.py
@@ -6,6 +6,7 @@ from collections import Counter
 from nameparser import HumanName
 import aiohttp
 import asyncio
+import time
 
 
 def load_email():
@@ -40,8 +41,8 @@ def detect_genders(results):
     d = gender.Detector(case_sensitive=False)
     genders = []
 
-    try:
-        for record in results:
+    for record in results:
+        try:
             for name in record['message']['author']:
 
                 name = HumanName(name['given'])
@@ -55,37 +56,44 @@ def detect_genders(results):
                     # Seems like a first name we can process
                     genders.append(d.get_gender(name.first))
 
-    except:
-        # No author record - possibly citing an organisation, or invalid DOI
-        pass
+        except:
+            # No author record - possibly citing an organisation, or invalid DOI
+            pass
 
     return genders
 
-async def fetch(session, url):
-    async with session.get(url) as response:
+async def fetch(session, url, headers):
+    async with session.get(url, headers=headers) as response:
         try:
             return await response.json()
         except:
             # DOI not found by crossref, or another http error.
-            print(response.status, response.text)
+            pass
 
 async def crossref_call(urls):
-    # headers = {'User-Agent': 'genderrefs/0.1 (https://swdg.io; mailto:{})'.format(load_email())}
+    headers = {'User-Agent': 'genderrefs/0.1 (https://swdg.io; mailto:{})'.format(load_email())}
     tasks = []
     async with aiohttp.ClientSession() as session:
-        for url in urls[:3]:
-            tasks.append(fetch(session, url))
+        for url in urls:
+            tasks.append(fetch(session, url, headers))
         responses = await asyncio.gather(*tasks)
         return responses
 
 
 refs = load_bibtex(sys.argv[1])
 queries = build_queries(refs)
-loop = asyncio.get_event_loop()
-results = loop.run_until_complete(crossref_call(queries))
+results = []
+
+# Here we split our parallel requests into chunks of 25, so we don't hit a rate limit
+for i in range(0, len(queries)+1, 25):
+    loop = asyncio.get_event_loop()
+    results += loop.run_until_complete(crossref_call(queries[i - 25: i]))
+    time.sleep(2)
+
 genders = detect_genders(results)
 
 print('\n\n\tFound {} references.'.format(len(refs.entries)))
+print('\t{} references have a DOI.'.format(len(queries)))
 print('\tReferences without a DOI cannot be processed.\n\n')
 
 counts = Counter(genders)
